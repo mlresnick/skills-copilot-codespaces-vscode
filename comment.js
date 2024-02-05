ac316c9c310a909e091c5746bde213b73da7266e
Resnick, Michael <michael.resnick@nuance.com>
2024-02-05 15:50:18 -0500

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,13 @@
+// Create web server
+var express = require('express');
+var app = express();
+
+// Create a route for the root path
+app.get('/', function(req, res) {
+  res.send('Hello, World!');
+});
+
+// Start the server on port 3000
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
